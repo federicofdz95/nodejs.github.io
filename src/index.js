2925b8098f7065afd5022f885e19879f03be1d28
federico <federicofdz95@gmail.com>
2021-06-23 10:23:56 -0300

    cambios

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const server = http.createServer(app);
 const path = require('path');
 
 
 // setting
-app.set('port',3000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
@@ -17,10 +18,10 @@ app.use(require('./routes/index'));
 
 
 // statics files
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // listening the server
 app.listen(app.get('port'), () => {
-    console.log('Server on port', app.get('port'));
+    console.log(`Server on port ${app.get('port')}`);
 });
